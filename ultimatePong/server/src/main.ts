5b4ea8e52e807ce68db8e8e83b68c30bc1cda97c
Tom Weimer <93084975+TomWeimer@users.noreply.github.com>
2023-01-06 15:30:24 +0100

    Husky, prettier and eslint

--- ultimatePong/server/src/main.ts
+++ ultimatePong/server/src/main.ts
@@ -3,7 +3,6 @@ import { AppModule } from "./app.module";
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
-    //   Cors activé car la connexion database s'effectue depuis le browser
     await app.listen(3000);
 }
 bootstrap();
