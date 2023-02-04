0e25ecd5ced3fe1e358274d17b14c96b3b15ca48
Bruno De Oliveira <bruno1388@Brunos-MacBook-Pro.local>
2023-02-04 04:55:40 +0100

    entities stilll in the making

--- server/src/chat/chat.module.ts
+++ server/src/chat/chat.module.ts
@@ -1,5 +1,11 @@
 import { Module } from "@nestjs/common";
 import { ChatGateway } from "../chat/chat.gateway";
+import { Channel } from "../typeorm/entities/Channel.entity";
+import { ChannelUser } from "../typeorm/entities/ChannelUser.entity";
+import { BlockedUser } from "../typeorm/entities/BlockedUser.entity";
+import { MutedUser } from "../typeorm/entities/MutedUser.entity";
+import { Message } from "../typeorm/entities/Message.entity";
+import { ChatController } from "./chat.controller";
 
 @Module({
     imports: [ChatGateway],
