5f047b59be24291ef17f2e00dfa94fba10d93b2a
Bruno De Oliveira <bruno1388@Brunos-MacBook-Pro.local>
2023-02-13 01:17:09 +0100

    deletion of loginForm and signupForm files

--- react-app/src/features/authentification/components/LogIn.tsx
+++ react-app/src/features/authentification/components/LogIn.tsx
@@ -1,7 +1,8 @@
 import style from "../styles/Login.module.css";
 import { SubmitButton } from "../../../components/SubmitButton";
 import { useNavigate } from "react-router-dom";
-import { LogInForm } from "./LoginForm";
+import { Input } from "../../../components/Input";
+import { useInput } from "../../../hooks/useInput";
 
 interface Props {
     divStyle: string;
@@ -9,10 +10,15 @@ interface Props {
 }
 
 export function Login() {
+    const username = useInput("", "Login", "login", "text", true);
+    const password = useInput("", "Password", "password", "password", true);
+
     const navigate = useNavigate();
     let divStyle: string = style.div_container;
 
     const joinChat = () => {
+        console.log(`login: ${username.value}`)
+        console.log(`login: ${password.value}`)
         navigate("/messages", { replace: true });
     };
 
@@ -26,18 +32,17 @@ export function Login() {
         navigate("/subscribe", { replace: true });
     };
 
-
     return (
         <div className={style.form_container}>
             <div className={style.div_container}>
                 <h3>Sign in to your account</h3>
             </div>
-            <LogInForm divStyle={divStyle} />
+            <Input divStyle={divStyle} content={username} />
+            <Input divStyle={divStyle} content={password} />
             <SubmitButton placeholder="Log In" divStyle={divStyle} onClick={joinChat} />
             <SubmitButton placeholder="No account? Sign up." buttonStyle={style.link_button} divStyle={divStyle} onClick={subscribe} />
             <hr className={style.hrLogin} />
             <SubmitButton placeholder="Sign in 42" divStyle={divStyle} onClick={signIn42} />
         </div>
     );
-
 }
--- react-app/src/features/authentification/components/SignUp.tsx
+++ react-app/src/features/authentification/components/SignUp.tsx
@@ -1,11 +1,17 @@
 import style from "../styles/SignUp.module.css";
 import { useNavigate } from "react-router-dom";
 import { SubmitButton } from "../../../components/SubmitButton";
-import { SignUpForm } from "./SignUpForm";
+import { Input } from "../../../components/Input";
+import { useInput } from "../../../hooks/useInput";
 
 export function SignUp() {
     const navigate = useNavigate();
     const divStyle = style.div_container;
+
+    const username = useInput("", "Login", "login", "text", true);
+    const mail = useInput("", "Email", "mail", "mail", true);
+    const pwd = useInput("", "Password", "pwd", "password", true);
+    const repwd = useInput("", "Re-type Password", "repwd", "password", true);
 
     const signup = () => {
         navigate("/", { replace: true });
@@ -16,7 +22,10 @@ export function SignUp() {
             <div className={style.div_container}>
                 <h3>Fill this subscription form</h3>
             </div>
-            <SignUpForm divStyle={divStyle} />
+            <Input divStyle={divStyle} content={username} />
+            <Input divStyle={divStyle} content={mail} />
+            <Input divStyle={divStyle} content={pwd} />
+            <Input divStyle={divStyle} content={repwd} />
             <SubmitButton placeholder="Subscribe" onClick={signup} divStyle={divStyle}/>
         </div>
     );
