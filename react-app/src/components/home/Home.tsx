96d64b8a60d70429344b625e99e0490b2fd75045
Bruno De Oliveira <bruno1388@Brunos-MacBook-Pro.local>
2023-03-13 09:23:16 +0100

    change in main age and socket provider

--- react-app/src/components/home/Home.tsx
+++ react-app/src/components/home/Home.tsx
@@ -1,20 +1,9 @@
-import { useNavigate } from "react-router-dom";
-import { FC, useEffect, useState } from "react";
 import { Navbar, Topbar, Settings, Friendbar } from ".";
 import Chat from "../chat/Chat";
-import { useAuth } from "../../context";
-import { useAxios, useInvitation } from "../../hooks";
-import { AxiosRequestConfig } from "axios";
-import { useLocalStorage } from "../../hooks/useLocalStorage";
-import Cookies from "js-cookie";
 import { useFeature, Feature } from "../../context/feature.context";
 import { ContactIcon } from "../../assets/images";
 import "./styles/home.scss";
-
-const defaultRequest: AxiosRequestConfig = {
-    method: "GET",
-    url: "/users/me",
-};
+import { useState } from "react";
 
 const featureComponent = new Map<number, JSX.Element>([
     [Feature.None, <></>],
@@ -25,45 +14,8 @@ const featureComponent = new Map<number, JSX.Element>([
 ]);
 
 function Home() {
-    const [request] = useState<AxiosRequestConfig>(defaultRequest);
-    const navigate = useNavigate();
-    const { userAuth } = useAuth();
-    const { setItem, getItem, removeItem } = useLocalStorage();
-    const { response, error } = useAxios(request);
     const { feature } = useFeature();
     const [friendsVisible, setFriendsVisible] = useState(false);
-
-    useInvitation();
-
-    useEffect(() => {
-        if (response !== undefined) {
-            const user = {
-                id: response.data.id,
-                username: response.data.username,
-                avatar: response.data.avatar,
-                authStrategy: response.data.authStrategy,
-                enable2FA: response.data.enable2FA,
-            };
-            if (JSON.stringify(user) !== getItem("user")) {
-                setItem("user", JSON.stringify(user));
-            }
-        }
-    }, [response]);
-
-    useEffect(() => {
-        if (error?.response?.status === 401) {
-            console.log(
-                "*** UNAUTHORIZED USE OF THE SITE - PLEASE SIGNIN IN ***"
-            );
-            removeItem("user");
-            Cookies.remove("JWTtoken", { sameSite: "none", secure: true });
-            navigate("/login");
-        }
-    }, [error]);
-
-    useEffect(() => {
-        console.log("AUTH user in homepage: ", userAuth);
-    }, [userAuth]);
 
     return (
         <div className="home">
--- react-app/src/pages/Main.tsx
+++ react-app/src/pages/Main.tsx
@@ -1,7 +1,54 @@
 import { SocketProvider } from "../context";
 import { Home } from "../components/home";
+import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useAuth } from "../context";
+import { useAxios } from "../hooks";
+import { useLocalStorage } from "../hooks/useLocalStorage";
+import { AxiosRequestConfig } from "axios";
+import Cookies from "js-cookie";
+
+const defaultRequest: AxiosRequestConfig = {
+    method: "GET",
+    url: "/users/me",
+};
 
 function Main() {
+    const [request] = useState<AxiosRequestConfig>(defaultRequest);
+    const navigate = useNavigate();
+    const { userAuth } = useAuth();
+    const { setItem, getItem, removeItem } = useLocalStorage();
+    const { response, error } = useAxios(request);
+
+    useEffect(() => {
+        if (response !== undefined) {
+            const user = {
+                id: response.data.id,
+                username: response.data.username,
+                avatar: response.data.avatar,
+                authStrategy: response.data.authStrategy,
+                enable2FA: response.data.enable2FA,
+            };
+            if (JSON.stringify(user) !== getItem("user")) {
+                setItem("user", JSON.stringify(user));
+            }
+        }
+    }, [response]);
+
+    useEffect(() => {
+        if (error?.response?.status === 401) {
+            console.log(
+                "*** UNAUTHORIZED USE OF THE SITE - PLEASE SIGNIN IN ***"
+            );
+            removeItem("user");
+            Cookies.remove("JWTtoken", { sameSite: "none", secure: true });
+            navigate("/login");
+        }
+    }, [error]);
+
+    useEffect(() => {
+        console.log("AUTH user in homepage: ", userAuth);
+    }, [userAuth]);
 
     return (
         <SocketProvider>
