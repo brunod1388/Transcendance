46993a34be1f2c91bf76594791ed3cfd9da7cacb
Tom Weimer <93084975+TomWeimer@users.noreply.github.com>
2023-04-09 14:41:10 +0200

    Matchmaking component

--- react-app/src/components/game/components/MatchBoard.tsx
+++ react-app/src/components/game/components/MatchBoard.tsx
@@ -3,34 +3,13 @@ import { MatchIcon } from "../../../assets/images";
 import "../styles/matchBoard.scss";
 import { useSocket } from "../../../hooks";
 import { useAuth } from "../../../context";
-
+import { Matchmaking } from "./Matchmaking";
 type Props = {};
 
 export default function MatchBoard({}: Props) {
-	const [isWaiting, setIsWaiting] = useState<boolean>(false);
-	const [socket] = useSocket();
-	const {userAuth} = useAuth();
-
-	useEffect(() => {
-		socket.emit("getStatusMatchmaking", userAuth.id);
-	}, []);
-
-	useEffect(() => {
-		socket.on("matchmakingStatus", (isWaiting: boolean) => setIsWaiting(isWaiting));
-		return () => {
-			socket.off("matchmakingStatus");
-		}
-	}, []);
-
     return (
         <div className="match-container">
-            <div className="button-container">
-                <div className="button-border" />
-                <button className="play-button" onClick={() => {
-					 socket.emit((isWaiting)? "leaveMatchmaking" : "joinMatchmaking", userAuth.id);
-					 socket.emit("getStatusMatchmaking", userAuth.id);
-				}}>{(isWaiting)? "Waiting" : "Play"}</button>
-            </div>
+			<Matchmaking/>
         </div>
     );
 }
---
+++ react-app/src/components/game/components/Matchmaking.tsx
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+import "../styles/matchBoard.scss";
+import { useSocket } from "../../../hooks";
+import { useAuth } from "../../../context";
+
+export function Matchmaking() {
+	const [isWaiting, setIsWaiting] = useState<boolean>(false);
+	const [socket] = useSocket();
+	const {userAuth} = useAuth();
+
+	useEffect(() => {
+		socket.emit("getStatusMatchmaking", userAuth.id);
+	}, []);
+
+	useEffect(() => {
+		socket.on("matchmakingStatus", (isWaiting: boolean) => setIsWaiting(isWaiting));
+		return () => {
+			socket.off("matchmakingStatus");
+		}
+	}, []);
+
+	return (<div className="button-container">
+	<div className="button-border" />
+	<button className="play-button" onClick={() => {
+		 socket.emit((isWaiting)? "leaveMatchmaking" : "joinMatchmaking", userAuth.id);
+		 socket.emit("getStatusMatchmaking", userAuth.id);
+	}}>{(isWaiting)? "Waiting" : "Play"}</button>
+</div>);
+}
